20dc97dc7f13620343622eddb8507337ecbecfe5
Serdar Sayın <serdarsayin@pm.me>
2023-05-04 16:56:35 +0300

    refactor: fix code formatting

--- include/common/masks.hpp
+++ include/common/masks.hpp
@@ -8,21 +8,21 @@
 #include <cstdint>
 
 namespace masks {
-static constexpr uint32_t sign_bit = 0x80000000U;
-static constexpr uint32_t lsb_zero = 0xFFFFFFFEU;
-static constexpr uint32_t msb_zero = 0x7FFFFFFFU;
+static constexpr uint32_t sign_bit   = 0x80000000U;
+static constexpr uint32_t lsb_zero   = 0xFFFFFFFEU;
+static constexpr uint32_t msb_zero   = 0x7FFFFFFFU;
 static constexpr uint32_t type_u_imm = 0xFFFFF000U;
 
 namespace tvec {
-static constexpr uint32_t type = 0x00000002U;
+static constexpr uint32_t type      = 0x00000002U;
 static constexpr uint32_t base_addr = 0xFFFFFFFCU;
 } // namespace tvec
 
-static constexpr uint32_t ecall = 0x73U;
+static constexpr uint32_t ecall  = 0x73U;
 static constexpr uint32_t ebreak = 0x9002U;
-static constexpr uint32_t mret = 0x30200073U;
-static constexpr uint32_t sret = 0x10200073U;
-static constexpr uint32_t wfi = 0x10500073U;
+static constexpr uint32_t mret   = 0x30200073U;
+static constexpr uint32_t sret   = 0x10200073U;
+static constexpr uint32_t wfi    = 0x10500073U;
 
 } // namespace masks
 
